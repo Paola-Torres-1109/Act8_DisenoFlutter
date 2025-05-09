c799dd7918c78143a4963820c7d41149113406ed
PAOLA_TORRES_VALLE <a.22308051281109@cbtis128.edu.mx>
2025-05-09 15:19:12 +0000

    ya

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,400 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 120,
+            padding: EdgeInsets.only(top: 50, right: 20, left: 20, bottom: 10),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.grey[200]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey),
+                        hintText: "Search",
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(
+                  Icons.camera_alt,
+                  color: Colors.grey[800],
+                  size: 30,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(
+                          "Stories",
+                          style: TextStyle(
+                              color: Colors.grey[900],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                              letterSpacing: 1.2),
+                        ),
+                        Text("See Archive"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 180,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          makeStory(
+                              storyImage: 'assets/bmw2.jpg',
+                              userImage: 'assets/bmw.jpg',
+                              userName: 'BMW Group'),
+                          makeStory(
+                              storyImage: 'assets/bocho.jpg',
+                              userImage: 'assets/vw.png',
+                              userName: 'Volkswagen'),
+                          makeStory(
+                              storyImage: 'assets/challenger.jpg',
+                              userImage: 'assets/dodge.png',
+                              userName: 'Dodge'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    makeFeed(
+                        userName: 'Pao Torres',
+                        userImage: 'assets/pao.jpeg',
+                        feedTime: '1 hr ago',
+                        feedText:
+                            'All the Lorem Ipsum generators on the Internet tend to repeat predefined.',
+                        feedImage: 'assets/camaro.jpg'),
+                    makeFeed(
+                        userName: 'Emi DSS',
+                        userImage: 'assets/emi.jpeg',
+                        feedTime: '3 mins ago',
+                        feedText:
+                            "All the Lorem Ipsum generators on the Internet tend to repeat predefined.All the Lorem Ipsum generators on the Internet tend to repeat predefined.All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
+                        feedImage: ''),
+                    makeFeed(
+                        userName: 'Luis Cervantes',
+                        userImage: 'assets/wicho.jpeg',
+                        feedTime: '3 mins ago',
+                        feedText:
+                            "All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
+                        feedImage: 'assets/zl1.jpg'),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+
+  Widget makeStory({storyImage, userImage, userName}) {
+    return AspectRatio(
+      aspectRatio: 1.6 / 2,
+      child: Container(
+        margin: EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image:
+              DecorationImage(image: AssetImage(storyImage), fit: BoxFit.cover),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.9),
+                Colors.black.withOpacity(.1),
+              ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                    image: DecorationImage(
+                        image: AssetImage(userImage), fit: BoxFit.cover)),
+              ),
+              Text(
+                userName,
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeFeed({userName, userImage, feedTime, feedText, feedImage}) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage(userImage), fit: BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        userName,
+                        style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Text(
+                        feedTime,
+                        style: TextStyle(fontSize: 15, color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.more_horiz,
+                  size: 30,
+                  color: Colors.grey[600],
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            feedText,
+            style: TextStyle(
+                fontSize: 15,
+                color: Colors.grey[800],
+                height: 1.5,
+                letterSpacing: .7),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          feedImage != ''
+              ? Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: AssetImage(feedImage), fit: BoxFit.cover)),
+                )
+              : Container(),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  makeLike(),
+                  Transform.translate(offset: Offset(-5, 0), child: makeLove()),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "2.5K",
+                    style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+                  )
+                ],
+              ),
+              Text(
+                "400 Comments",
+                style: TextStyle(fontSize: 13, color: Colors.grey[800]),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              makeLikeButton(isActive: true),
+              makeCommentButton(),
+              makeShareButton(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget makeLike() {
+    return Container(
+      width: 25,
+      height: 25,
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.white)),
+      child: Center(
+        child: Icon(Icons.thumb_up, size: 12, color: Colors.white),
+      ),
+    );
+  }
+
+  Widget makeLove() {
+    return Container(
+      width: 25,
+      height: 25,
+      decoration: BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.white)),
+      child: Center(
+        child: Icon(Icons.favorite, size: 12, color: Colors.white),
+      ),
+    );
+  }
+
+  Widget makeLikeButton({isActive}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade200),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.thumb_up,
+              color: isActive ? Colors.blue : Colors.grey,
+              size: 18,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "Like",
+              style: TextStyle(color: isActive ? Colors.blue : Colors.grey),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeCommentButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade200),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.chat, color: Colors.grey, size: 18),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "Comment",
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeShareButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade200),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.share, color: Colors.grey, size: 18),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "Share",
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
         ),
       ),
     );
